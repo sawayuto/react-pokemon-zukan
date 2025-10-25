3dbaa576f1128cb264ca828f2bdd67902112d226
sawayuto <ktc25a32j0001@edu.kyoto-tech.ac.jp>
2025-10-25 12:23:56 +0900

    feat: add GitHub Actions workflow for deploying to GitHub Pages and configure routing in main.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
